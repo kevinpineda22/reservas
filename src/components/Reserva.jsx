7510812615a77bead0b37f738a597803eac39082
kevin <pintegrador201@gmail.com>
2024-11-12 15:31:35 -0500

    volvemos ruta de backend de render

--- src/components/Reserva.jsx
+++ src/components/Reserva.jsx
@@ -33,7 +33,7 @@ function ReservaForm() {
     const reservaData = { nombre, cedula, fecha, horaInicio, horaFinal, salon, telefono, correo };
 
     // Enviar la reserva al backend
-    fetch('http://129.123.150:3000/reservar', {
+    fetch('https://reservas-zer3.onrender.com/reservar', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
