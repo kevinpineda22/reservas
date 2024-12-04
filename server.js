bff9f4a09fe8d0a705c78da7c4a816710e9b3318
desarrollador software <desarrollo@merkahorrosas.com>
2024-12-04 15:49:35 -0500

    continuamos trabajando en mejorar de alerta y la logica

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/reservar', async (req, res) => {
     `;
 
     await pool.query(insertQuery, [nombre, area ,motivo, fecha, horaInicio, horaFinal,'reservado',salon]);
-    res.json({ mensaje: 'Reserva realizada con éxitodsd' });
+    res.json({ mensaje: 'Reserva almacenada exitosamente' });
 
   } catch (error) {
     console.error('Error al realizar la reserva:', error.message);
