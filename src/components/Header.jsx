91a3fb69a9f85dafb9c4fb1ff0fd2cf3f5924ec4
desarrollador software <desarrollo@merkahorrosas.com>
2024-12-10 14:41:30 -0500

    corregimos los nombre de los salones de reserva

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,15 @@ const Header = () => {
     <header className="header">
       <h1>Reserva tus espacios</h1>
       <div className="image-container">
-        <img src="/img1.jpeg" alt="Imagen 1" className="header-image" loading="lazy" />
-        <p className="image-title">Auditorio Principal</p>
-      </div>
-      
-      <div className="image-container">
         <img src="/img2.jpeg" alt="Imagen 2" className="header-image" loading="lazy" />
+        <p className="image-title">Auditorio Principal</p>
+      </div> 
+      <div className="image-container">
+        <img src="/img1.jpeg" alt="Imagen 1" className="header-image" loading="lazy" />
         <p className="image-title">Sala de Juntas</p>
       </div>
+      
+    
      
 
       {/* Enlace a /reservas */}
